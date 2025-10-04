9c4fd75d27a4be6f4e426545d61d8ed7414e84da
Nardos Tatek <narditatek@gmail.com>
2025-10-04 14:38:01 +0200

    add number of blocks and batchsize for direct use

--- layers/model_config.py
+++ layers/model_config.py
@@ -1,9 +1,11 @@
 class ModelConfig:
     vocab_size = 4000
-    T = 5                  
+    T = 10                  
     local_lr = 1e-5         
     clamp_value = 0.01        
     n_embed = 64            
     block_size = 128          
     dropout = 0.1
     num_heads = 4
+    n_layer = 4
+    batch_size=8
