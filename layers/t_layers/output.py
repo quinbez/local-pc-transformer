88763631d5fc0b5511064771ae2ab5cff1de93e9
quinbez <bezawitt9@gmail.com>
2025-10-07 12:57:48 +0300

    refactor output layer (change variable t to step to make it more descriptive)

--- layers/t_layers/output.py
+++ layers/t_layers/output.py
@@ -14,6 +14,11 @@ class Output(nn.Module):
         self.fc = nn.Linear(config.n_embed, config.vocab_size, bias=False)
         self.pc_layer = PCOutput(T=config.T, local_lr=config.local_lr)
 
-    def forward(self, target, t, requires_update: bool = True):
-        mu = self.pc_layer(layer=self.fc, target=target, t=t, requires_update=requires_update)
+    def forward(self, target, step, requires_update: bool = True):
+        mu = self.pc_layer(
+            layer=self.fc, 
+            target=target, 
+            step=step, 
+            requires_update=requires_update
+        )
         return mu
